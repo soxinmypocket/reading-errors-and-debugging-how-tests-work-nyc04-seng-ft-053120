d32dba4fb327fd831ba45f9529b210e1e259532c
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-07 02:48:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,1 @@
 # Add your variables here
-first_number = 
-
-second_number = 
-
-sum = first_number + second_number
-
-difference = first_number - second_number
-
-product = first_number * second_number
-
-quotient = first_number / second_number
