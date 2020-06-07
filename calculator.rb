b48cdbf671f8671f0208432f34066e132b97ff2f
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-07 03:01:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
 # Add your variables here
+
+first_number = expect(first_number).to be_an(Integer).or be_a(Float)
+
+second_number =
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
